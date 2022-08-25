fe1839e773299ab4743e3ee77503b8764a6cbed3
nemsothea <nemsothea13@gmail.com>
2022-08-25 15:01:29 +0900

    Initial Commit

---
+++ SimpleMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMVP
+//
+//  Created by MacBook Pro on 25/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
